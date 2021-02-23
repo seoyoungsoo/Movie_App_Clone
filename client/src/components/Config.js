8d5a448f0f14f8bc05a6ae316ab171a9fdd3f59a
seoyoungsoo <iulove37@naver.com>
2021-02-24 02:01:21 +0900

    Feat: TMDB API 추가
    - TMDB API 추가

--- client/src/components/Config.js
+++ client/src/components/Config.js
@@ -1,2 +1,6 @@
 //SERVER ROUTES
 export const USER_SERVER = '/api/users';
+
+export const API_URL = 'https://api.themoviedb.org/3/';
+export const IMG_BASE_URL = 'https://image.tmdb.org/t/p/';
+export const API_KEY = 'bc88316b138330662811650dddf8c016';
